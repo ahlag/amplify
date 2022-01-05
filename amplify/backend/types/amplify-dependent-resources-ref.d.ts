7bd4e215e194fb0104e8c3773f5e4fb57c7d8591
Chang Yong Lik <theeahlag@gmail.com>
2022-01-05 22:50:08 +0900

    first commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "amplifyFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "amplifyAPI": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
